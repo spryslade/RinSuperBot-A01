bc89932ea33146fbdb78459622d911a0841bac78
Xscythe-1240 <106799100+Xscythe-1240@users.noreply.github.com>
2022-09-02 11:49:10 +0530

    Add files via upload

---
+++ RinSuperBot/hacking_script.py
@@ -0,0 +1,19 @@
+PM_START_TEXT = """
+
+      ────「 Raiden Shogun 」────
+
+*Damm darling uh - {} so hot.*
+
+*Myself Rin,a model student and idol of Shirou's school.*
+
+➢ *Uptime* - {}
+➢ *Functioning among {} Users in {} Chats*
+
+*• @RinSuperBot is Bish of* [𝗔𝘀𝘁𝗼𝗿](https://t.me/AstorSupport)
+
+"""
+
+PM_START_PIC = "https://telegra.ph/file/23a2e22dcc2187f6478fd.jpg"
+
+## Don't Remove That Credits in PM_START_TEXT if yoy removed your app will be crashed.
+
